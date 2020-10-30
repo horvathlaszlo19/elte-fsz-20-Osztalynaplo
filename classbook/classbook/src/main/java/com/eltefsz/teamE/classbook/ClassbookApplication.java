30acfb5c70492676c9459964cbe62aed172cfbf1
horvath19 <hlaszlo826@gmail.com>
2020-10-30 16:12:20 +0100

    base fix

---
+++ classbook/classbook/src/main/java/com/eltefsz/teamE/classbook/ClassbookApplication.java
@@ -0,0 +1,13 @@
+package com.eltefsz.teamE.classbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassbookApplication.class, args);
+	}
+
+}
---
+++ classbook/classbook/src/test/java/com/eltefsz/teamE/classbook/ClassbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eltefsz.teamE.classbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
