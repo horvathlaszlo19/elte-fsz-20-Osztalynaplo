ec6a8eccbe2731a954af3e018116bb66c96aee7c
nmadyx <nmadyx@gmail.com>
2020-11-10 21:46:41 +0100

    Person nem Entity hanem MappedSuperclass

--- classbook/classbook/src/main/java/com/eltefsz/teamE/classbook/domain/Person.java
+++ classbook/classbook/src/main/java/com/eltefsz/teamE/classbook/domain/Person.java
@@ -1,12 +1,12 @@
 package com.eltefsz.teamE.classbook.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 
-@Entity
+@MappedSuperclass
 public class Person {
 
 	@Id
