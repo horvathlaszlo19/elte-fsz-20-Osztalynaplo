5a6b4d39f983412d1008d9b9d56df48abf28a324
nmadyx <nmadyx@gmail.com>
2020-11-16 00:28:48 +0100

    Repository update

--- classbook/classbook/src/main/java/com/eltefsz/e/classbook/repository/StudentRepository.java
+++ classbook/classbook/src/main/java/com/eltefsz/e/classbook/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.eltefsz.e.classbook.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.eltefsz.e.classbook.domain.Student;
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Long>{
 
+	Optional<Student> findByUsernameAndPassword(String username, String password);
+	
 }
