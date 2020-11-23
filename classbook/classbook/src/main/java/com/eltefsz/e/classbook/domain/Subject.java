207e66d5e4b1c23633bcec92c904deb2cb321893
horvath19 <hlaszlo826@gmail.com>
2020-11-23 11:31:41 +0100

    subject domain

--- classbook/classbook/src/main/java/com/eltefsz/e/classbook/domain/Subject.java
+++ classbook/classbook/src/main/java/com/eltefsz/e/classbook/domain/Subject.java
@@ -2,6 +2,19 @@ package com.eltefsz.e.classbook.domain;
 
 public enum Subject {
 
-	MATEK, IRODALOM, NYELVTAN, TORTENELEM, FOLDRAJZ, KEMIA, FIZIKA, TESTNEVELES
+	MATEK("Matek"), IRODALOM("Irodalom"), NYELVTAN("Nyelvtan"), TORTENELEM("Történelem"), FOLDRAJZ("Földrajz"), KEMIA("Kémia"), FIZIKA("Fizzika"), TESTNEVELES("Testnevelés");
+	
+	private final String name;
+	
+	Subject(String name){
+		this.name=name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+	
 	
 }
